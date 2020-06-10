197c14c03633d6e2d096e87109ece2798a41cde6
Jonathan Law <lawjonathan17@gmail.com>
2020-06-10 13:46:01 -0700

    Add ASCII85Decode

--- reader.go
+++ reader.go
@@ -4,14 +4,16 @@ import (
 	"bufio"
 	"bytes"
 	"compress/zlib"
+	"encoding/ascii85"
 	"encoding/binary"
 	"fmt"
-	"github.com/pkg/errors"
 	"io"
 	"io/ioutil"
 	"math"
 	"os"
 	"strconv"
+
+	"github.com/pkg/errors"
 )
 
 type PdfReader struct {
@@ -1423,11 +1425,21 @@ func (this *PdfReader) rebuildContentStream(content *PdfValue) ([]byte, error) {
 			// Uncompress zlib compressed data
 			var out bytes.Buffer
 			zlibReader, _ := zlib.NewReader(bytes.NewBuffer(stream))
+
 			defer zlibReader.Close()
 			io.Copy(&out, zlibReader)
 
 			// Set stream to uncompressed data
 			stream = out.Bytes()
+		case "/ASCII85Decode":
+			encoded := stream
+			// the -3 strips the end of data marker
+			decodedBytes, err := ioutil.ReadAll(ascii85.NewDecoder(bytes.NewBuffer(encoded[:len(encoded)-3])))
+			if err != nil {
+				fmt.Println("decoding ascii85:", err)
+			}
+			stream = decodedBytes
+
 		default:
 			return nil, errors.New("Unspported filter: " + filters[i].Token)
 		}
